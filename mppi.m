9dc44a61f60cba3875d668e0d971a3ec8c2b824a
acxz <17132214+acxz@users.noreply.github.com>
2019-06-06 20:46:21 -0400

    added the alpha term functionality as well as plot and print flags

--- mppi.m
+++ mppi.m
@@ -8,19 +8,12 @@ function [x_hist, u_hist, time_hist] = mppi(func_is_task_complete,
   func_control_update_converged, func_comp_weights, func_term_cost,
   func_run_cost,func_gen_next_ctrl, func_state_est, func_apply_ctrl, func_g,
   func_F, num_samples, learning_rate, init_state, init_ctrl_seq,
-  ctrl_noise_covar, time_horizon, per_ctrl_based_ctrl_noise)
+  ctrl_noise_covar, time_horizon, per_ctrl_based_ctrl_noise, plot_traj, print)
 
-  % initialize prereqs for algorithm
   % TODO check inputs for correct dimensionality and value ranges
   % TODO SGF
-  % TODO alpha
   % TODO comments lol
   % TODO provide compute parameterized compute weights function?
-  % TODO running cost does not have control should it? I mean we are
-  % generating state trajectories not control trajectories but like the
-  % control allows for that state trajectory to happen. Like if we have a
-  % cheap state trajectory at the cost of a high control then should we
-  % still forget about it in the trajectory cost.
 
   % Time stuff
   num_timesteps = size(init_ctrl_seq,2);
@@ -47,10 +40,15 @@ function [x_hist, u_hist, time_hist] = mppi(func_is_task_complete,
 
   % control sequence
   u_traj = init_ctrl_seq;
+  
+  % sampled control trajectories
+  v_traj = zeros(control_dim, num_samples, num_timesteps);
 
   % Plot trajectory in real time
-  figure(1)
-  hold on
+  if(plot_traj)
+    figure(1)
+    hold on
+  end
 
   total_timestep_num = 1;
   while(func_is_task_complete(xo, time) == false)
@@ -67,21 +65,32 @@ function [x_hist, u_hist, time_hist] = mppi(func_is_task_complete,
       flat_distribution = randn(control_dim, num_samples * num_timesteps);
       ctrl_noise_flat = ctrl_noise_covar * flat_distribution;
       ctrl_noise = reshape(ctrl_noise_flat, [control_dim, num_samples, num_timesteps]);
-
-      %v_traj = repmat(reshape(u_traj, [control_dim, 1, num_timesteps]), [1, num_samples, 1]) + ctrl_noise;
+      
+      ctrl_based_ctrl_noise_samples = round(per_ctrl_based_ctrl_noise * num_samples);
+      if (ctrl_based_ctrl_noise_samples == 0)
+        v_traj = ctrl_noise;
+      elseif (ctrl_based_ctrl_noise_samples == num_samples)
+        v_traj = repmat(reshape(u_traj, [control_dim, 1, num_timesteps]), [1, num_samples, 1]) + ctrl_noise;
+      else
+        v_traj(:,1:ctrl_based_ctrl_noise_samples,:) = repmat(reshape(u_traj, [control_dim, 1, num_timesteps]), [1, ctrl_based_ctrl_noise_samples, 1]) + ctrl_noise(:,1:ctrl_based_ctrl_noise_samples,:);
+        v_traj(:,ctrl_based_ctrl_noise_samples+1:end,:) = ctrl_noise(:,ctrl_based_ctrl_noise_samples+1:end,:);
+      end
+      
       for timestep_num = 1:num_timesteps
-        v_traj = u_traj(:,timestep_num) + ctrl_noise(:,:,timestep_num);
+        %v_traj = u_traj(:,timestep_num) + ctrl_noise(:,:,timestep_num);
 
         % TODO (maybe faster) According to algorithm we dont even need to save the x_traj trajectories across timesteps
-        x_traj(:,:,timestep_num+1) = func_F(x_traj(:,:,timestep_num),func_g(v_traj),dt);
-        %x_traj(:,:,timestep_num+1) = func_F(x_traj(:,:,timestep_num),func_g(v_traj(:,:,timestep_num)),dt);
+        %x_traj(:,:,timestep_num+1) = func_F(x_traj(:,:,timestep_num),func_g(v_traj),dt);
+        x_traj(:,:,timestep_num+1) = func_F(x_traj(:,:,timestep_num),func_g(v_traj(:,:,timestep_num)),dt);
         %x_sample_values(:,:) = func_F(x_sample_values(:,:),func_g(v),dt);
 
-        traj_cost += func_run_cost(x_traj(:,:,timestep_num+1)) + learning_rate * (u_traj(:,timestep_num)' * (inverse(ctrl_noise_covar) * v_traj));
-        %traj_cost += func_run_cost(x_traj(:,:,timestep_num+1)) + learning_rate * (u_traj(:,timestep_num)' * (inverse(ctrl_noise_covar) * v_traj(:,:,timestep_num)));
+        %traj_cost += func_run_cost(x_traj(:,:,timestep_num+1)) + learning_rate * (u_traj(:,timestep_num)' * (inverse(ctrl_noise_covar) * v_traj));
+        traj_cost += func_run_cost(x_traj(:,:,timestep_num+1)) + learning_rate * (u_traj(:,timestep_num)' * (inverse(ctrl_noise_covar) * v_traj(:,:,timestep_num)));
         %traj_cost += func_run_cost(x_sample_values(:,:)) + learning_rate * (u_traj(:,timestep_num)' * (inverse(ctrl_noise_covar) * v));
 
-        fprintf("TN: %d, IN: %d, DU: %d, Simtime: %d\n", timestep_num, iteration, mean(sum(abs(du),1)), time);
+        if(print)
+          fprintf("TN: %d, IN: %d, DU: %d, Simtime: %d\n", timestep_num, iteration, mean(sum(abs(du),1)), time);
+        end
       end
       % TODO investiage the speedup here
       %traj_cost = func_run_cost(x_traj(:,:,timestep_num+1)) + learning_rate * (u_traj(:,timestep_num)' * (inverse(ctrl_noise_covar) * v_traj(:,:,timestep_num)));
@@ -107,21 +116,25 @@ function [x_hist, u_hist, time_hist] = mppi(func_is_task_complete,
     time_hist = [time_hist, time];
 
     % Real time plotting
-    xlim([0,time_hist(end)])
-    state_colors = ['b', 'r', 'm', 'c'];
-    ctrl_colors = ['k', 'g', 'y', 'w'];
-    for sd = 1:state_dim
-      plot(time_hist(total_timestep_num:total_timestep_num+1),
-          x_hist(sd,total_timestep_num:total_timestep_num+1)',state_colors(mod(sd - 1,size(state_colors,2)) + 1))
-    end
-
-    if (total_timestep_num > 1)
-      for cd = 1:control_dim
-        plot(time_hist(total_timestep_num-1:total_timestep_num),
-            u_hist(cd,total_timestep_num-1:total_timestep_num)',ctrl_colors(mod(cd - 1,size(ctrl_colors,2)) + 1))
+    if(plot_traj)
+      xlim([0,time_hist(end)])
+      state_colors = ['b', 'r', 'm', 'c'];
+      ctrl_colors = ['k', 'g', 'y', 'w'];
+      for sd = 1:state_dim
+        plot(time_hist(total_timestep_num:total_timestep_num+1),
+             x_hist(sd,total_timestep_num:total_timestep_num+1)',
+             state_colors(mod(sd - 1,size(state_colors,2)) + 1))
       end
+
+      if (total_timestep_num > 1)
+        for cd = 1:control_dim
+          plot(time_hist(total_timestep_num-1:total_timestep_num),
+               u_hist(cd,total_timestep_num-1:total_timestep_num)',
+               ctrl_colors(mod(cd - 1,size(ctrl_colors,2)) + 1))
+        end
+      end
+      drawnow
     end
-    drawnow
 
     total_timestep_num = total_timestep_num + 1;
 
