4cc0ef4a5b330ac114cf31f2ca3b7ba3ca659af1
acxz <17132214+acxz@users.noreply.github.com>
2019-06-07 10:23:07 -0400

    added comments removed unnessecary lines

--- mppi.m
+++ mppi.m
@@ -13,10 +13,9 @@ function [x_hist, u_hist, time_hist] = mppi(func_is_task_complete,
 
   % TODO check inputs for correct dimensionality and value ranges
   % TODO SGF
-  % TODO comments lol
   % TODO provide compute parameterized compute weights function?
-  
-  % Time stuff
+
+  % time stuff
   num_timesteps = size(init_ctrl_seq,2);
   dt = time_horizon/num_timesteps;
   time = 0;
@@ -36,17 +35,16 @@ function [x_hist, u_hist, time_hist] = mppi(func_is_task_complete,
   %du = realmax * ones(control_dim, num_timesteps);
   du = 100 * ones(control_dim, num_timesteps);
 
-  % State trajectories
+  % state trajectories
   x_traj = zeros(state_dim, num_samples, num_timesteps + 1);
-  %x_sample_values = zeros(state_dim, num_samples);
 
   % control sequence
   u_traj = init_ctrl_seq;
-  
+
   % sampled control trajectories
   v_traj = zeros(control_dim, num_samples, num_timesteps);
-  
-  % Plot trajectory in real time
+
+  % plot trajectory in real time
   if(plot_traj)
     figure(1)
     hold on
@@ -55,20 +53,21 @@ function [x_hist, u_hist, time_hist] = mppi(func_is_task_complete,
   total_timestep_num = 1;
   while(func_is_task_complete(xo, time) == false)
 
+    % state estimation at the next timestep
     xo = func_state_est(true_x);
     x_traj(:,:,1) = repmat(xo,[1, num_samples]);
-    %x_sample_values(:,:) = repmat(xo,[1, num_samples]);
 
     iteration = 1;
     while(func_control_update_converged(du, iteration) == false)
 
-      traj_cost = zeros(1, num_samples);
-      traj_cost += repmat(func_run_cost(xo), [1, num_samples]);
+      traj_cost = repmat(func_run_cost(xo), [1, num_samples]);
 
+      % Noise generation
       flat_distribution = randn(control_dim, num_samples * num_timesteps);
       ctrl_noise_flat = ctrl_noise_covar * flat_distribution;
       ctrl_noise = reshape(ctrl_noise_flat, [control_dim, num_samples, num_timesteps]);
-      
+
+      % Compute sampled control trajectories
       ctrl_based_ctrl_noise_samples = round(per_ctrl_based_ctrl_noise * num_samples);
       if (ctrl_based_ctrl_noise_samples == 0)
         v_traj = ctrl_noise;
@@ -78,36 +77,30 @@ function [x_hist, u_hist, time_hist] = mppi(func_is_task_complete,
         v_traj(:,1:ctrl_based_ctrl_noise_samples,:) = repmat(reshape(u_traj, [control_dim, 1, num_timesteps]), [1, ctrl_based_ctrl_noise_samples, 1]) + ctrl_noise(:,1:ctrl_based_ctrl_noise_samples,:);
         v_traj(:,ctrl_based_ctrl_noise_samples+1:end,:) = ctrl_noise(:,ctrl_based_ctrl_noise_samples+1:end,:);
       end
-      
+
       for timestep_num = 1:num_timesteps
-        %v_traj = u_traj(:,timestep_num) + ctrl_noise(:,:,timestep_num);
 
-        % TODO (maybe faster) According to algorithm we dont even need to save the x_traj trajectories across timesteps
-        %x_traj(:,:,timestep_num+1) = func_F(x_traj(:,:,timestep_num),func_g(v_traj),dt);
+        % Forward propagation
         x_traj(:,:,timestep_num+1) = func_F(x_traj(:,:,timestep_num),func_g(v_traj(:,:,timestep_num)),dt);
-        %x_sample_values(:,:) = func_F(x_sample_values(:,:),func_g(v),dt);
 
-        %traj_cost += func_run_cost(x_traj(:,:,timestep_num+1)) + learning_rate * (u_traj(:,timestep_num)' * (inverse(ctrl_noise_covar) * v_traj));
         traj_cost += func_run_cost(x_traj(:,:,timestep_num+1)) + learning_rate * (u_traj(:,timestep_num)' * (inverse(ctrl_noise_covar) * v_traj(:,:,timestep_num)));
-        %traj_cost += func_run_cost(x_sample_values(:,:)) + learning_rate * (u_traj(:,timestep_num)' * (inverse(ctrl_noise_covar) * v));
 
         if(print)
           fprintf("TN: %d, IN: %d, DU: %d, Simtime: %d\n", timestep_num, iteration, mean(sum(abs(du),1)), time);
         end
       end
-      
+
       if(save_sampling)
         save("-append", [sampling_filename '_v_traj.dat'],'v_traj');
         save("-append", [sampling_filename '_x_traj.dat'],'x_traj');
         save("-append", [sampling_filename '_traj_cost.dat'], 'traj_cost');
       end
-      
+
       % TODO investiage the speedup here
       %traj_cost = func_run_cost(x_traj(:,:,timestep_num+1)) + learning_rate * (u_traj(:,timestep_num)' * (inverse(ctrl_noise_covar) * v_traj(:,:,timestep_num)));
       traj_cost += func_term_cost(x_traj(:,:,timestep_num+1));
-      %traj_cost += func_term_cost(x_sample_values(:,:));
 
-
+      % Weight and du calculation
       w = func_comp_weights(traj_cost);
       du = reshape(sum(repmat(w, [control_dim, 1, num_timesteps]) .* ctrl_noise,2), [control_dim, num_timesteps]);
 
@@ -116,14 +109,16 @@ function [x_hist, u_hist, time_hist] = mppi(func_is_task_complete,
 
     end
 
-    u_traj(1:end-1) = u_traj(2:end);
-    u_traj(end) = func_gen_next_ctrl(u_traj(end));
-
+    % Apply control and log data
     true_x = func_apply_ctrl(x_hist(:,total_timestep_num), u_traj(:,1), dt);
     x_hist(:,total_timestep_num+1) = true_x;
     u_hist = [u_hist u_traj(:,1)];
     time = time + dt;
     time_hist = [time_hist, time];
+
+    % Warmstart next control trajectory using past generated control trajectory
+    u_traj(1:end-1) = u_traj(2:end);
+    u_traj(end) = func_gen_next_ctrl(u_traj(end));
 
     % Real time plotting
     if(plot_traj)
