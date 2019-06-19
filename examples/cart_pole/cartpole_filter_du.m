9d70874e91fd005ffaf2f67f6406f00dc76e3b8f
acxz <17132214+acxz@users.noreply.github.com>
2019-06-19 16:18:41 -0400

    Added capability to filter du

---
+++ examples/cart_pole/cartpole_filter_du.m
@@ -0,0 +1,8 @@
+% Author: Akash Patel (apatel435)
+% Date: 6/6/19
+
+function retval = cartpole_filter_du(du)
+
+    retval = du;
+
+end
---
+++ examples/inv_pen/inv_pen_filter_du.m
@@ -0,0 +1,8 @@
+% Author: Akash Patel (apatel435)
+% Date: 5/29/19
+
+function retval = inv_pen_filter_du(du)
+
+    retval = du;
+
+end
